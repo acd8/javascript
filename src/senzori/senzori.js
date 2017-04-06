a6964f66132562b39f366b97eb965b68a8462147
acd <alex.donea92@gmail.com>
2017-04-06 19:48:18 +0300

    Update senzor miscare

--- src/senzori/senzori.js
+++ src/senzori/senzori.js
@@ -7,4 +7,13 @@ function on_device_orientation(evt){
  document.getElementById("a").innerHTML ="Alpha="+Math.round(alpha).toString();
   document.getElementById("b").innerHTML = "Beta="+Math.round(beta).toString();
   document.getElementById("c").innerHTML = "Gamma="+Math.round(gamma).toString();
+  var canvas=document.getElementById("canvas");
+  var ctx= canvas.getContext("2d");
+ ctx.clearRect(0,0, canvas.width,canvas.height);
+  var raza=20;
+  var centru={x:canvas.width/2, y:canvas.height/2};
+  ctx.beginPath();
+ctx.arc(centru.x +gamma*(canvas.width /2-raza), centru.y+beta*(canvas.height /2-raza), raza, 0, 2 * Math.PI);
+ctx.stroke();
+
 }
