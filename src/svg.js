717e4510fd40a367ec7f0c27163dd9ca2c15bc90
acd <alex.donea92@gmail.com>
2017-05-04 18:33:18 +0300

    update v2 svg

--- src/svg.js
+++ src/svg.js
@@ -2,8 +2,9 @@ var c1=document.getElementById("cerc1");
 c1.addEventListener("touchmove", on_touch_move);
 
 
-function on_touch_move(){
+function on_touch_move(e){
 	var t=e.changedTouches[0];
 	var x=c1.getAttribute("cx");
-	alert(x);
+	c1.setAttribute("cx",t.pageX);
+	c1.setAttribute("cy",t.pageY);
 }
